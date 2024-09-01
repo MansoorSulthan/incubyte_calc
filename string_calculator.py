7db9c9af93ace36e5dc74bff22e5d751b75c91e8
Mansoor Sulthan <mansoor.sulthan@royalcyber.com>
2024-09-01 20:39:25 +0530

    formatted the code with 'Black'

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,3 @@
-
 def add(numbers):
     if numbers == "":
         return 0
@@ -12,10 +11,7 @@ def add(numbers):
     nums = [int(num) for num in nums if num]
     negatives = [num for num in nums if num < 0]
     if negatives:
-        raise ValueError(f"Negative numbers not allowed: {', '.join(map(str, negatives))}")
+        raise ValueError(
+            f"Negative numbers not allowed: {', '.join(map(str, negatives))}"
+        )
     return sum(nums)
-
-
-
-#
-#
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,6 +1,7 @@
 import unittest
 from string_calculator import add
 
+
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
