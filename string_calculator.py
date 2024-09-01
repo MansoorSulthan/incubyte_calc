71e4d71c4fed8b80860faaef6fa6c3e8700e0481
Mansoor Sulthan <mansoor.sulthan@royalcyber.com>
2024-09-01 20:24:19 +0530

    added custom delimiter handling strings to sum in calculator

--- string_calculator.py
+++ string_calculator.py
@@ -1,7 +1,12 @@
 def add(numbers):
     if numbers == "":
         return 0
-    numbers = numbers.replace("\n", ",")
+    if numbers.startswith("//"):
+        delimiter = numbers[2]
+        numbers = numbers[4:]
+        numbers = numbers.replace(delimiter, ",")
+    else:
+        numbers = numbers.replace("\n", ",")
     nums = map(int, numbers.split(","))
     return sum(nums)
 
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -14,6 +14,12 @@ class TestStringCalculator(unittest.TestCase):
     def test_multiple_numbers(self):
         self.assertEqual(add("1,2,3,4"), 10)
 
+    def test_newline_delimiters(self):
+        self.assertEqual(add("1\n2,3"), 6)
+
+    def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
